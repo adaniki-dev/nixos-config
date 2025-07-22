cb25254e3486124c59e12e1e8328b8f4c0f3020c
adaniki-dev <ikiyoshikai@gmail.com>
2025-07-22 09:27:28 -0300

    updated nixos

--- system/home.nix
+++ system/home.nix
@@ -23,8 +23,14 @@
     xdg.configFile."kitty/kitty.conf".source = ./../desktop/hyprland/kitty/kitty.conf;
     xdg.configFile."waybar/config".source = ./../desktop/hyprland/waybar/config;
     xdg.configFile."waybar/style.css".source = ./../desktop/hyprland/waybar/style.css;
-    xdg.configFile."rofi/config.rasi".source = ./../desktop/hyprland/rofi/config.rasi;
-	xdg.configFile."rofi/powermenu.sh".source = ./../desktop/hyprland/rofi/powermenu.sh;
+    xdg.configFile."rofi/powermenu.sh".source = ./../desktop/hyprland/rofi/powermenu.sh;
     xdg.configFile."rofi/powermenu.rasi".source = ./../desktop/hyprland/rofi/powermenu.rasi;
+    xdg.configFile."rofi/menu.rasi".source = ./../desktop/hyprland/rofi/menu.rasi;
+    xdg.configFile."rofi/config.rasi".source = ./../desktop/hyprland/rofi/config.rasi;
+    xdg.configFile."rofi/colors.rasi".source = ./../desktop/hyprland/rofi/colors.rasi;
+    xdg.configFile."rofi/fonts.rasi".source = ./../desktop/hyprland/rofi/fonts.rasi;
+    xdg.configFile."rofi/catppuciccin.rasi".source = ./../desktop/hyprland/rofi/catppuciccin.rasi;
+    xdg.configFile."kitty/current-theme.conf".source = ./../desktop/hyprland/kitty/current-theme.conf;
+    xdg.configFile."kitty/kitty.conf.bak".source = ./../desktop/hyprland/kitty/kitty.conf.bak;
   };
 }
